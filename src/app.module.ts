3588b3ac0158eb16639c4988ae2c3c35b5c852c4
Pierre_T <pierre.tsiakkaros@gmail.com>
2025-06-15 12:43:42 +0200

    Initialize NestJS project with TypeScript, ESLint, Prettier, and i18n support.

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AcceptLanguageResolver, I18nModule } from 'nestjs-i18n';
+import * as path from 'path';
+
+@Module({
+  imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '..', 'i18n'),
+        watch: true,
+      },
+      resolvers: [AcceptLanguageResolver],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}

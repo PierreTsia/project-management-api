21c7aef4927f0d711f99f3f2ce3a4415a55cfcf8
Pierre_T <pierre.tsiakkaros@gmail.com>
2025-06-15 13:12:36 +0200

    Revamp CI/CD pipeline and enhance type safety: Added GitHub Actions for continuous integration, introduced type checking in package.json, and refactored AppController and AppService to leverage i18n for dynamic message translation.

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { I18nService } from 'nestjs-i18n';
 
 describe('AppController', () => {
   let appController: AppController;
+  let i18nService: I18nService;
 
   beforeEach(async () => {
+    const mockI18nService = {
+      translate: jest.fn().mockImplementation((key) => key),
+    };
+
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: I18nService,
+          useValue: mockI18nService,
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    i18nService = app.get<I18nService>(I18nService);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return translated message', async () => {
+      const result = await appController.getHello();
+      expect(result).toBe('test.day_interval');
+      expect(i18nService.translate).toHaveBeenCalledWith('test.day_interval', {
+        args: {
+          count: 3,
+        },
+      });
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { I18nService } from 'nestjs-i18n';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly i18n: I18nService) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return this.i18n.translate('test.day_interval', {
+      args: {
+        count: 3,
+      },
+    });
   }
 }

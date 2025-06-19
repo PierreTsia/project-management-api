0f133cca0e97520a59d6bcccf632315baf50f25b
Pierre_T <pierre.tsiakkaros@gmail.com>
2025-06-19 21:41:01 +0200

    feat: complete Task 1.2 - integrate ProjectContributor and ProjectPermissionService into module

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -3,12 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
 import { Project } from './entities/project.entity';
+import { ProjectContributor } from './entities/project-contributor.entity';
+import { ProjectPermissionService } from './services/project-permission.service';
 import { LoggerModule } from '../common/services/logger.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project]), LoggerModule],
+  imports: [
+    TypeOrmModule.forFeature([Project, ProjectContributor]),
+    LoggerModule,
+  ],
   controllers: [ProjectsController],
-  providers: [ProjectsService],
-  exports: [ProjectsService],
+  providers: [ProjectsService, ProjectPermissionService],
+  exports: [ProjectsService, ProjectPermissionService],
 })
 export class ProjectsModule {}
